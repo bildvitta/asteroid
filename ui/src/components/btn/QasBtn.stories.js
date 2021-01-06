c9986a55306468d2ccc033d0d05dc97206dffeb2
Lourengato <lourengato@gmail.com>
2021-01-06 12:57:24 -0300

    adding stories

---
+++ ui/src/components/btn/QasBtn.stories.js
@@ -0,0 +1,42 @@
+import QasBtn from './QasBtn.vue'
+
+export default {
+  title: 'Components/Button',
+  component: QasBtn,
+
+  argTypes: {
+    noCaps: {
+      name: 'noCaps',
+      description: 'Boolean for caps in label'
+    },
+
+    color: {
+      name: 'color',
+      description: 'Receives a string for the button color'
+    },
+
+    unelevated: {
+      name: 'unelevated',
+      description: 'Boolean for elevation'
+    },
+
+    hideMobileLabel: {
+      name: 'hideMobileLabel',
+      description: 'Hides button label in mobile, showing only the icon'
+    }
+  }
+}
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { QasBtn },
+  template:
+   `
+   <qas-btn v-bind="$props">
+      Click Here
+   </qas-btn>
+   
+    `
+})
+
+export const Default = Template.bind({})
