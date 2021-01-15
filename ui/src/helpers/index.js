e744d32315241f32b4c274ea7661b466506689e7
DouglasCalora <douglascalora123@gmail.com>
2021-01-15 10:34:03 -0300

    GridGenerator Documentation

---
+++ ui/src/helpers/index.js
@@ -0,0 +1,30 @@
+import {
+  asset,
+  booleanLabel,
+  date,
+  dateTime,
+  formatCompanyDocument,
+  formatPersonalDocument,
+  handleDocument,
+  handleMasks,
+  humanDate,
+  money,
+  optionLabel,
+  percent
+} from './filters.js'
+
+export {
+  // filters
+  asset,
+  booleanLabel,
+  date,
+  dateTime,
+  formatCompanyDocument,
+  formatPersonalDocument,
+  handleDocument,
+  handleMasks,
+  humanDate,
+  money,
+  optionLabel,
+  percent
+}
