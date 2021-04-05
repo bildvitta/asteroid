47736cb2f6f83aca4f38da6923734ddddcf3210a
DouglasCalora <douglascalora123@gmail.com>
2021-04-05 15:35:51 -0300

    removed unseless code

--- ui/dev/src/pages/Test1.vue
+++ ui/dev/src/pages/Test1.vue
@@ -1,32 +1,10 @@
 <template>
   <q-page padding>
-    {{ teste }}
-    <div>
-    <q-form @submit.prevent="() => null">
-    <qas-input ref="input" v-model="teste" :rules="[value => value.length || 'obrigatorio']" />
-    <qas-btn @click="validate" type="submit">clique aqui</qas-btn>
-    </q-form>
-    </div>
   </q-page>
 </template>
 
 <script>
-import vuexStoreModule from '../helpers/vuexStoreModule'
-
-export default {
-  data () {
-    return {
-      teste: ''
-    }
-  },
-
-  methods: {
-    validate () {
-      const input = this.$refs.input
-
-    }
-  }
-}
+export default {}
 </script>
 
 <style lang="sass" scoped>
