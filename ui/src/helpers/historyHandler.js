324b5cf654e33ca929308755457fc3cbac327375
Hebert Silva <hebertpereirasilva@gmail.com>
2021-04-28 11:04:01 -0300

    fix: remove extra arrow function

--- ui/src/helpers/historyHandler.js
+++ ui/src/helpers/historyHandler.js
@@ -35,7 +35,7 @@ function handleHistory () {
         return null
       }
 
-      routes.forEach((route) => {
+      routes.forEach(route => {
         const index = history.list.findIndex(item => item.name === route)
 
         if (~index) {
