fa8ad9614472cbbe68c6cec486a6f3a605c3d092
Leonardo Marco <leonardo.silva.marco@gmail.com>
2021-04-29 10:58:08 -0300

    QasTooltip and QasTabs arrows layout

---
+++ ui/src/components/tooltip/QasTooltip.stories.js
@@ -0,0 +1,49 @@
+import QasTooltip from './QasTooltip.vue'
+
+export default {
+  component: QasTooltip,
+  title: 'Components/Tooltip',
+
+  parameters: {
+    docs: {
+      description: {
+        component: 'Creates a warning or message will be shown.'
+      }
+    }
+  },
+
+  argTypes: {
+    // Props
+    color: {
+      description: 'Icon\'s color name.'
+    },
+
+    message: {
+      description: 'Tooltip\'s message.'
+    },
+
+    messageIcon: {
+      description: 'Message\'s icon name.'
+    },
+
+    messageIconSize: {
+      description: 'Message\'s icon size.'
+    },
+
+    mobileMessageProps: {
+      description: 'Props of $q.notify plugin'
+    },
+
+    size: {
+      description: 'Tooltip\'s icon size.'
+    }
+  }
+}
+
+const Template = (args, { argTypes }) => ({
+  components: { QasTooltip },
+  props: Object.keys(argTypes),
+  template: '<qas-tooltip v-bind="$props" message-icon="o_assignment_late" :mobile-message-props="{message: `Mobile notify example!`, icon:`o_assignment_late`}" messageIcon="o_assignment_late" icon="o_assignment_late" message="Message!" />'
+})
+
+export const Default = Template.bind({})
---
+++ ui/src/components/tooltip/QasTooltip.vue
@@ -0,0 +1,67 @@
+<template>
+  <q-icon :color="color" :name="icon" :size="size" @click="showMobileNotify(mobileMessageProps)">
+    <q-tooltip v-if="!$_isSmall" anchor="top middle" self="bottom middle">
+      <div class="no-wrap row">
+        <q-icon v-if="messageIcon" :name="messageIcon" :size="messageIconSize" />
+        <div class="q-ml-sm text-subtitle2">{{ message }}</div>
+      </div>
+    </q-tooltip>
+  </q-icon>
+</template>
+
+<script>
+import screen from '../../mixins/screen.js'
+
+export default {
+  mixins: [screen],
+
+  props: {
+    color: {
+      default: 'primary',
+      type: String
+    },
+
+    icon: {
+      default: '',
+      type: String,
+      required: true
+    },
+
+    message: {
+      default: '',
+      type: String,
+      required: true
+    },
+
+    messageIcon: {
+      default: '',
+      type: String
+    },
+
+    messageIconSize: {
+      default: '20px',
+      type: String
+    },
+
+    mobileMessageProps: {
+      default: () => ({}),
+      type: Object
+    },
+
+    size: {
+      default: '20px',
+      type: String
+    }
+  },
+
+  methods: {
+    showMobileNotify (attrs) {
+      if (this.$_isSmall) {
+        this.$q.notify({
+          ...attrs
+        })
+      }
+    }
+  }
+}
+</script>
