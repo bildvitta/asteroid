6012db77e832e8465854866b3764686d797171d4
Caio Tarifa <caiotarifa@gmail.com>
2021-05-11 01:02:12 -0300

    Border classes and storybook fix.

--- ui/dev/src/pages/TestDesignSystem.vue
+++ ui/dev/src/pages/TestDesignSystem.vue
@@ -1,11 +1,22 @@
 <template>
   <q-page class="container flex flex-center spaced">
-    <div class="shadow-primary">box shadow</div>
+    <div>
+      <qas-box form-mode>
+        <div>Primary: <input v-model="primaryColor" type="color"> ({{ primaryColor }})</div>
+        <div>Secondary: <input v-model="secondaryColor" type="color"> ({{ secondaryColor }}</div>
+      </qas-box>
 
-    <q-radio v-model="radio" label="Radio" val="1" />
-    <q-radio v-model="radio" label="Ou não?" val="2" />
+      <qas-box>
+        <div class="shadow-primary">box shadow</div>
 
-    <q-btn label="change color" @click="changeColor" />
+        <q-radio v-model="radio" label="Radio" val="1" />
+        <q-radio v-model="radio" label="Ou não?" val="2" />
+
+        <br>
+
+        <q-btn color="primary" label="Random Colors" @click="changeColor" />
+      </qas-box>
+    </div>
   </q-page>
 </template>
 
@@ -15,14 +26,31 @@ import { colors } from 'quasar'
 export default {
   data () {
     return {
+      primaryColor: '',
+      secondaryColor: '',
+
       radio: ''
+    }
+  },
+
+  watch: {
+    primaryColor () {
+      colors.setBrand('primary', this.primaryColor)
+    },
+
+    secondaryColor () {
+      colors.setBrand('secondary', this.secondaryColor)
     }
   },
 
   methods: {
     changeColor () {
-      const color = Math.floor(Math.random() * 16777215).toString(16)
-      colors.setBrand('primary', '#' + color)
+      this.primaryColor = '#' + this.getRandomColor()
+      this.secondaryColor = '#' + this.getRandomColor()
+    },
+
+    getRandomColor () {
+      return Math.floor(Math.random() * 16777215).toString(16)
     }
   }
 }
