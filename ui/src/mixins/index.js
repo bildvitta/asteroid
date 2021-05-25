1a29c18d4bc20d048c404b5f37ca5e651832b8db
Leonardo Marco <leonardo.silva.marco@gmail.com>
2021-05-25 10:42:01 -0300

    fix mixins

--- ui/src/mixins/index.js
+++ ui/src/mixins/index.js
@@ -3,13 +3,11 @@ import generatorMixin from './generator.js'
 import passwordMixin from './password.js'
 import screenMixin from './screen.js'
 import viewMixin from './view.js'
-import filterObjectMixin from './filter-object.js'
 
 export {
   contextMixin,
   generatorMixin,
   passwordMixin,
   screenMixin,
-  viewMixin,
-  filterObjectMixin
+  viewMixin
 }
