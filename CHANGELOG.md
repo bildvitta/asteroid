fb4dbab477aba94c257a4bf61d2277b03a5d5031
Leonardo Marco <leonardo.silva.marco@gmail.com>
2021-05-25 13:30:33 -0300

    add prop margin in qas-label

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,11 +6,14 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## 2.x.x - 2021-05-21
 
+### Added
+
+- Added margin prop in QasLabel
+
 ### Removed
 
 - Remove q-pa-sm actions slot in QasCard
 - Remove require prop QasLabel 
-- Remove margin in QasLabel
 - Remove position absolute in QasSettings
 ### Fixed 
 - iconSize in QasSettings
--- ui/src/components/label/QasLabel.vue
+++ ui/src/components/label/QasLabel.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="text-bold text-subtitle2" v-bind="$attrs" v-on="$listeners">
+  <div class="text-bold text-subtitle2" :class="labelClasses" v-bind="$attrs" v-on="$listeners">
     <!-- TODO: Remover camelCase. -->
     <slot :formattedLabel="formattedLabel">
       {{ formattedLabel }}
@@ -17,6 +17,11 @@ export default {
       type: String
     },
 
+    margin: {
+      default: 'sm',
+      type: String
+    },
+
     quantity: {
       default: 0,
       type: Number
@@ -26,6 +31,10 @@ export default {
   computed: {
     formattedLabel () {
       return formatLabel(this.quantity, this.label)
+    },
+
+    labelClasses () {
+      return `q-mb-${this.margin}`
     }
   }
 }
