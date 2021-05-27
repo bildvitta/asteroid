d352a176aa7d21d3db3c7ba12b99d99cff48edb7
Leonardo Marco <leonardo.silva.marco@gmail.com>
2021-05-27 13:46:21 -0300

    prop width qasTooltip

--- ui/src/components/tooltip/QasTooltip.vue
+++ ui/src/components/tooltip/QasTooltip.vue
@@ -1,7 +1,7 @@
 <template>
   <q-icon :color="color" :name="icon" :size="size" @click="showNotify(notifyProps)">
     <q-tooltip v-if="!$_isSmall" anchor="top middle" self="bottom middle">
-      <div class="no-wrap row">
+      <div class="items-center no-wrap row" :style="tooltipStyle">
         <q-icon v-if="messageIcon" :name="messageIcon" :size="messageIconSize" />
         <div class="q-ml-sm text-subtitle2">{{ message }}</div>
       </div>
@@ -51,6 +51,19 @@ export default {
     size: {
       default: '20px',
       type: String
+    },
+
+    width: {
+      default: '300px',
+      type: String
+    }
+  },
+
+  computed: {
+    tooltipStyle () {
+      return {
+        width: this.width
+      }
     }
   },
 
