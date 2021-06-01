f332d00ad1f9fec652da8e0629644c7e5c8b3d17
Leonardo Marco <leonardo.silva.marco@gmail.com>
2021-06-01 11:33:52 -0300

    fixes

--- ui/src/components/map/QasMap.stories.js
+++ ui/src/components/map/QasMap.stories.js
@@ -40,22 +40,6 @@ export default {
 
     zoom: {
       description: 'Define the initial zoom.'
-    },
-
-    // Slots
-    actions: {
-      description: 'Bottom section for actions.',
-      table: noSummary
-    },
-
-    default: {
-      description: 'Main content.',
-      table: noSummary
-    },
-
-    header: {
-      description: 'Header content.',
-      table: noSummary
     }
   }
 }
@@ -64,62 +48,26 @@ const Template = (args, { argTypes }) => ({
   components: { QasMap },
   props: Object.keys(argTypes),
   template:
-  `<div class="q-col-gutter-md q-pa-md row">
-    <qas-Map :images="images" use-header>
-      <template #default>
-        <div>{{ result.name }}</div>
-        <div>{{ result.email }}</div>
-      </template>
-      <template #actions>
-        <q-icon color="primary" class="col-grow" name="o_edit" />
-        <q-icon color="primary" class="col-grow" name="o_delete" />
-        <q-icon color="primary" class="col-grow" name="o_photo_camera" />
-      </template>
-    </qas-Map>
-    <qas-Map :images="images" use-header>
-      <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
-    </qas-Map>
-    <qas-Map :images="images" use-header>
-      <template #default>
-        <div>{{ result.name }}</div>
-        <div>{{ result.email }}</div>
-      </template>
-      <template #actions>
-        <div class="full-width">
-          <div class="text-blue text-bold text-center">Ver mais</div>
-        </div>
-      </template>
-    </qas-Map>
-  </div>`
+  '<qas-map :center-focus="{lat: -21.17663, lng: -47.820839 }" has-search :markers="$_createMarker(marker, true)" show-info-window />'
 })
 
-const images = [
-  'https://cdn.pixabay.com/photo/2016/10/10/12/54/space-1728314_960_720.jpg',
-  'https://cdn.pixabay.com/photo/2011/12/14/12/21/orion-nebula-11107_960_720.jpg',
-  'https://cdn.pixabay.com/photo/2016/10/10/12/54/space-1728314_960_720.jpg',
-  'https://cdn.pixabay.com/photo/2011/12/14/12/21/orion-nebula-11107_960_720.jpg'
-]
-
-const result = {
-  name: 'John Appleseed',
-  email: 'john.appleseed@example.com',
-  phone: '99999999999',
-  gender: 'Male',
-  document: '99999999999',
-  address: '747 Austen View'
+const marker = {
+  position: { lat: -21.17663, lng: -47.820839 },
+  title: 'Teste do componente QasMap',
+  description: 'Ribeirão Preto',
+  draggable: true
 }
 
 export const Default = Template.bind({})
 Default.args = {
-  images,
-  result
+  marker
 }
 
 Default.parameters = {
   docs: {
     source: {
       code:
-      '<qas-Map :images="images" :result="result" />'
+      '<qas-map :center-focus="{lat: -21.17663, lng: -47.820839 }" has-search :markers="$_createMarker(marker, true)" show-info-window  />'
     }
   }
 }
