40bc0a34d21e03bf8d0b860fc5083723c24a61d6
Guilherme Queiróz <g.aqueiroz19@gmail.com>
2021-08-25 11:08:31 -0300

    fixing map-markers icon import

--- ui/src/mixins/map-markers.js
+++ ui/src/mixins/map-markers.js
@@ -11,7 +11,7 @@ export default {
           title: name,
           description: city,
           draggable: !index && this.$_isEditMode,
-          icon: index ? require('images/marker.png') : ''
+          icon: referencePoint.icon
         })
       })
         
