840c52f230f8c96c36916abaffa4d9a9bca424d9
eliasassuncao <elias3748@gmail.com>
2021-08-26 14:26:19 -0300

    remove comment

--- ui/dev/src/pages/TestSignaturePad.vue
+++ ui/dev/src/pages/TestSignaturePad.vue
@@ -1,13 +1,13 @@
 <template>
   <q-page class="container row spaced">
     <div class="col-12">
-      <!-- <qas-signature-pad ref="signaturePad" height="250" :is-empty.sync="isEmpty" :options="{ backgroundColor }" />
+      <qas-signature-pad ref="signaturePad" height="250" :is-empty.sync="isEmpty" :options="{ backgroundColor }" />
       <div v-if="isEmpty" class="q-mt-sm">Está vazio</div>
       <div class="q-gutter-sm q-mb-xl q-mt-sm row">
         <q-btn color="primary" label="Salvar" no-caps @click="saveSignature" />
         <q-btn color="primary" label="Limpar" no-caps @click="clearSignature" />
         <q-btn color="primary" label="Trocar cor" no-caps @click="changeColor" />
-      </div> -->
+      </div>
 
       <qas-signature-uploader v-model="signature" class="col-12" entity="portal-app/problems/photo" signature-label="Assinatura do Cleyton" upload-label="Assinatura digital" />
       <pre class="q-mb-xl">v-model {{ signature }}</pre>
