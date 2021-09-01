82257c050149d05f58e23a4848f283b52750cdb0
eliasassuncao <elias3748@gmail.com>
2021-09-01 17:40:57 -0300

    create computed attributes

--- ui/src/components/select/QasSelect.vue
+++ ui/src/components/select/QasSelect.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-select v-model="selectModel" v-bind="$attrs" clearable emit-value map-options :options="filteredOptions" outlined :use-input="searchable" v-on="$listeners" @filter="filterOptions">
+  <q-select v-model="selectModel" v-bind="attributes" :options="filteredOptions" :use-input="searchable" v-on="$listeners" @filter="filterOptions">
     <slot v-for="(slot, key) in $slots" :slot="key" :name="key" />
 
     <template v-for="(slot, key) in $scopedSlots" :slot="key" slot-scope="scope">
@@ -99,6 +99,16 @@ export default {
 
     isMultiple () {
       return this.$attrs.multiple || this.$attrs.multiple === ''
+    },
+
+    attributes () {
+      return {
+        emitValue: true,
+        mapOptions: true,
+        outlined: true,
+        clearable: true,
+        ...this.$attrs
+      }
     }
   },
 
