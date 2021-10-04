0f6317b951f0fa78585f209f5434a03dbb90ab9e
Douglas Calora <douglascalora123@gmail.com>
2021-10-04 20:05:55 -0300

    added docs

--- ui/src/components/uploader/QasCustomUpload.vue
+++ ui/src/components/uploader/QasCustomUpload.vue
@@ -19,7 +19,7 @@ export default {
   },
 
   computed: {
-    // overrides isBusy from quasar for adding "this.isAddingFiles"
+    // overrides "isBusy" from quasar for adding "this.isAddingFiles"
     isBusy () {
       return this.isAddingFiles || this.promises.length > 0
     },
@@ -35,32 +35,44 @@ export default {
   },
 
   methods: {
+    // function for handling files and resize if it is an image type
     async filesHandler (file) {
       const { type } = file
 
+      // only resize if is an image type
       if (!type.includes('image')) return file
-
+      
+      // create a new image and canvas for resizing purpose
       const image = new Image()
       image.src = URL.createObjectURL(file)
 
       const canvas = document.createElement('canvas')
+
+      // get the original width and height of the uploaded image
       const { width, height } = await this.getImageSize(image)
+
+      // get the redimensioned sizes
       const resizedDimensions = this.resizeDimensions(1280, width, height)
 
       canvas.width = resizedDimensions.width
       canvas.height = resizedDimensions.height
 
+      // fix cors issues
       image.crossOrigin = ''
+
       image.width = width
       image.height = height
 
+      // get the resized canvas and transform it into a blob
       const resizedImage = await pica().resize(image, canvas, this.picaResizeOptionsDefault)
       const blob = await (pica().toBlob(resizedImage, type, 0.90))
 
+
+      // now returns the newly formatted file
       return new File([blob], file.name)
     },
 
-    // overrides __getInputControl from quasar
+    // overrides "__getInputControl" from quasar
     __getInputControl (h) {
       return [
         h('input', {
@@ -73,6 +85,7 @@ export default {
             ...(this.multiple === true ? { multiple: true } : {})
           },
           on: {
+            // here is where the magic works
             change: async ({ target }) => {
               this.files = []
               this.isAddingFiles = true
