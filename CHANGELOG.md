49ef5f3dd4300a6cd5143ce0ced850af75b6058b
Douglas Calora <douglascalora123@gmail.com>
2021-12-22 14:06:00 -0300

    Added `div` as parent tag in `QasDecimalInput` for rules validation when submiting a form.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## Unreleased
+
+### Changed
+- Added `div` as parent tag in `QasDecimalInput` for rules validation when submiting a form.
+
 ## 2.12.3 - 2021-12-10
 
 ### Fixed
---
+++ ui/dev/src/pages/TestDecimalInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-page class="container">
+    <q-form @submit.prevent>
+      <qas-decimal-input v-model="model" hint="Insira um valor maior ou igual 10" :rules="[customValidator]" />
+
+      <div class="q-my-lg">
+        model -> {{ model }}
+      </div>
+
+      <q-btn type="submit">Validar rules!</q-btn>
+    </q-form>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      model: 0
+    }
+  },
+
+  methods: {
+    customValidator (value) {
+      return value > '10.00' || 'valor precisa ser maior ou igual a 10'
+    }
+  }
+}
+</script>
