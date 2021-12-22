16f3c87d2664bd407fb0fd24a96970ec5de36a29
Douglas Calora <douglascalora123@gmail.com>
2021-12-22 14:40:34 -0300

    more changes for testing

--- ui/dev/src/pages/TestDecimalInput.vue
+++ ui/dev/src/pages/TestDecimalInput.vue
@@ -1,7 +1,14 @@
 <template>
   <q-page class="container">
     <q-form @submit.prevent>
-      <qas-decimal-input v-model="model" hint="Insira um valor maior ou igual 10" :rules="[customValidator]" />
+      <div class="q-col-gutter-md row">
+        <div class="col-6">
+          <qas-decimal-input v-model="model" hint="Insira um valor maior ou igual 10" :rules="[customValidator]" />
+        </div>
+        <div class="col-6">
+          <qas-decimal-input v-model="model2" hint="Insira um valor maior ou igual 10" :rules="[customValidator]" />
+        </div>
+      </div>
 
       <div class="q-my-lg">
         model -> {{ model }}
@@ -16,7 +23,8 @@
 export default {
   data () {
     return {
-      model: 0
+      model: 0,
+      model2: 0
     }
   },
 
