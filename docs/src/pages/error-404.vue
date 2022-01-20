b7a7aa434697a886b64f45ec38571b26819b4300
Caio Tarifa <caiotarifa@gmail.com>
2022-01-20 15:33:21 -0300

    configuring stylelint

--- docs/src/pages/error-404.vue
+++ docs/src/pages/error-404.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="bg-blue flex flex-center fullscreen q-pa-md text-center text-white">
     <div>
-      <div style="font-size: 30vh">
+      <div style="font-size: 30vh;">
         404
       </div>
 
-      <div class="text-h2" style="opacity:.4">
+      <div class="text-h2" style="opacity: 0.4;">
         Oops. Nothing here...
       </div>
 
