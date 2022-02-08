444eb6ab9220a16476be3f2fd3bc85338004f3f5
douglascalora123@gmail.com <douglascalora123@gmail.com>
2022-02-08 16:15:24 -0300

    Finished QasTableGenerator

--- ui/src/components/table-generator/QasTableGenerator.vue
+++ ui/src/components/table-generator/QasTableGenerator.vue
@@ -1,13 +1,15 @@
 <template>
-  <q-table ref="table" class="bg-transparent qas-table-generator" :class="tableClass" v-bind="attributes">
-    <template v-for="(_, name) in $slots" #[name]="context">
-      <slot v-if="hasBodySlot" name="body" :props="context" />
+  <qas-box class="q-mb-xl q-px-lg q-py-md">
+    <q-table ref="table" class="bg-transparent qas-table-generator" :class="tableClass" v-bind="attributes">
+      <template v-for="(_, name) in $slots" #[name]="context">
+        <slot v-if="hasBodySlot" name="body" :props="context" />
 
-      <q-td v-else :key="name">
-        <slot :name="name" v-bind="context || {}" />
-      </q-td>
-    </template>
-  </q-table>
+        <q-td v-else :key="name">
+          <slot :name="name" v-bind="context || {}" />
+        </q-td>
+      </template>
+    </q-table>
+  </qas-box>
 </template>
 
 <script>
