3baccec95e000dbf1cfc1ee0174cb8b8205dede9
douglascalora123@gmail.com <douglascalora123@gmail.com>
2022-04-26 14:28:09 -0300

    added Forbidden and NotFound Page

---
+++ ui/src/pages/Forbidden.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="fixed-center text-center">
+    <p class="q-my-lg text-faded">Ops, você não tem permissão para acessar esta página <strong>(403).</strong></p>
+    <q-btn color="primary" flat @click="$router.push('/')">Voltar</q-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Forbidden'
+}
+</script>
---
+++ ui/src/pages/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="fixed-center text-center">
+    <p class="q-my-lg text-faded">Ops, nada aqui... <strong>(404)</strong></p>
+    <q-btn color="primary" flat @click="$router.push('/')">Voltar</q-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound'
+}
+</script>
