a07cff286c8e693d79a5ee1ca54a960b2ef24d7e
douglascalora123@gmail.com <douglascalora123@gmail.com>
2022-04-27 18:35:55 -0300

    fixed call of method addRoute

--- app-extension/src/boot/history.js
+++ app-extension/src/boot/history.js
@@ -2,7 +2,7 @@ import useHistory from '@bildvitta/quasar-ui-asteroid/src/composables/useHistory
 
 export default ({ router }) => {
   router.beforeEach((to, from, next) => {
-    const addRoute = useHistory()
+    const { addRoute } = useHistory()
     addRoute(to)
     next()
   })
