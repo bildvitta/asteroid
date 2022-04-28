c346f01a0d2b0eadb8694e3587bde524bec01b65
douglascalora123@gmail.com <douglascalora123@gmail.com>
2022-04-28 13:27:51 -0300

    added doc

--- ui/src/plugins/screen/Screen.js
+++ ui/src/plugins/screen/Screen.js
@@ -2,10 +2,15 @@ import { Screen, Platform } from 'quasar'
 
 export default () => {
   const screensModel = {
+    // até 599px
     isSmall: () => Screen.xs,
+    // de 600 até 1023px
     isMedium: () => Screen.sm,
+    // de 600 até 1023px
     isLarge: () => Screen.gt.sm,
+    // de 0 até 599px
     untilMedium: () => Screen.lt.sm,
+    // de 0 ate 1023px
     untilLarge: () => Screen.lt.md,
 
     // Plataforma
