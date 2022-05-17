b9bf6176ae4d7f3ee95e6543f846e4d6f91aa714
Rafael Martins de Castro <42079080+Far3llProject@users.noreply.github.com>
2022-05-16 23:33:34 -0300

    update index page and add favicons

--- docs/src/pages/index.vue
+++ docs/src/pages/index.vue
@@ -1,7 +1,45 @@
 <template>
   <doc-page>
-    <div class="q-pa-lg">
-      Olá, universo!
+    <div class="column items-center q-gutter-y-lg q-mt-lg text-center text-grey-7">
+      <q-img src="../assets/logo/dashboard_logo.png" width="300px" />
+      <q-badge color="orange" :label="version" />
+      <div>
+        Asteroid é uma extensão do Quasar contendo coleções de componentes, plugins, estilos, utilitários, focado na produtividade.
+      </div>
+      <div class="q-gutter-sm">
+        <q-btn color="primary" label="Começando" to="/start/usage" unelevated />
+        <q-btn color="primary" label="Porque existe?" outline to="/why-asteroid" />
+        <q-btn color="black" href="https://github.com/bildvitta/asteroid" label="github" target="blank" unelevated />
+      </div>
+
+      <div class="column q-gutter-y-sm">
+        <div class="text-bold text-primary">
+          Doação
+        </div>
+        <div>
+          Se você aprecia o trabalho que encontrou neste projeto, considere doar para o
+          <a href="https://quasar.dev" target="blank">Quasar Framework</a>.
+        </div>
+        <div class="text-bold text-primary">
+          Licença
+        </div>
+        <div>
+          <span class="non-selectable"> MIT (c) Caio Tarifa </span>
+          <span class="text-primary"> caiotarifa@gmail.com </span>
+        </div>
+      </div>
     </div>
   </doc-page>
 </template>
+
+<script>
+import { version } from 'asteroid'
+
+export default ({
+  computed: {
+    version () {
+      return version
+    }
+  }
+})
+</script>
