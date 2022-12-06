3523a10b8c19f8b3211921c71a9f023a4939dcbf
eliasassuncao <elias3748@gmail.com>
2022-12-06 19:35:12 -0300

    feat(QasNotify): created close button on notify/changed notify position

--- ui/src/plugins/notify-success/NotifySuccess.js
+++ ui/src/plugins/notify-success/NotifySuccess.js
@@ -1,10 +1,25 @@
 import { Notify } from 'quasar'
+import Screen from '../screen/Screen.js'
 
 Notify.registerType('success', {
-  icon: 'o_check',
+  icon: 'o_check_circle',
   progress: true
 })
 
 export default (message, caption) => {
-  Notify?.create({ caption, message, type: 'success' })
+  const marginValues = `margin-top: ${Screen().untilMedium ? '40px' : '80px'};`
+  
+  Notify?.create({
+    caption,
+    message,
+    type: 'success',
+    position: 'top',
+    attrs: {
+      style: marginValues
+    },
+    closeBtn: 'x',
+    timeout: 10000000
+  })
+
+
 }
