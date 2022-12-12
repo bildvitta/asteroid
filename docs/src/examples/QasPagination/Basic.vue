bbc17f23f573f5e6af551b7cb8948e1edca3cf4f
Caio Tarifa <caiotarifa@gmail.com>
2022-12-12 14:36:15 -0300

    pagination

---
+++ docs/src/examples/QasPagination/Basic.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container spaced">
+    <qas-pagination v-model="model" :max="50" />
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      model: 1
+    }
+  }
+}
+</script>
