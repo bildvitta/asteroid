bf17fa5a3dde7ca931aeba190d490e6c271af8b9
Caio Tarifa <caiotarifa@gmail.com>
2022-12-20 14:55:40 -0300

    docs for QasAppUser

---
+++ docs/src/examples/QasAppUser/Basic.vue
@@ -0,0 +1,19 @@
+<template>
+  <qas-app-user :user="user" />
+</template>
+
+<script>
+const user = {
+  photo: 'https://www.abc.net.au/cm/lb/6367016/data/alan2c-see-the-person-data.jpg',
+  name: 'Eduardo Lima',
+  email: 'eduardolima@gmail.com'
+}
+
+export default {
+  computed: {
+    user () {
+      return user
+    }
+  }
+}
+</script>
--- ui/src/components/app-user/QasAppUser.vue
+++ ui/src/components/app-user/QasAppUser.vue
@@ -77,7 +77,7 @@ export default {
 
     user: {
       default: () => ({}),
-      require: true,
+      required: true,
       type: Object
     }
   },
