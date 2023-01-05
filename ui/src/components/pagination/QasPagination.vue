6703048afc468a28c9630505878af510d441826b
Douglas Calora <douglascalora123@gmail.com>
2023-01-05 11:22:28 -0300

    finished changes

--- ui/src/components/pagination/QasPagination.vue
+++ ui/src/components/pagination/QasPagination.vue
@@ -15,6 +15,7 @@ export default {
         activeDesign: 'flat',
         boundaryNumbers: true,
         color: 'grey-8',
+        class: 'qas-pagination',
         directionLinks: true,
         maxPages: modelValue < 3 ? 3 : 6,
         modelValue,
@@ -25,3 +26,11 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.qas-pagination {
+  .q-icon {
+    color: $grey-9;
+  }
+}
+</style>
