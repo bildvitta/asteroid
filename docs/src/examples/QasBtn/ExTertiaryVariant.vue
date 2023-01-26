db3e87d55de68a630e5594ec2dfcdade65656e73
Douglas Calora <douglascalora123@gmail.com>
2023-01-26 11:49:09 -0300

    work in progress

---
+++ docs/src/examples/QasBtn/ExTertiaryVariant.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-mb-lg">
+      <div class="q-gutter-sm q-mb-lg">
+        <qas-label>Habilitado</qas-label>
+        <qas-btn label="tertiary" variant="tertiary" />
+        <qas-btn icon="sym_r_search" label="tertiary" variant="tertiary" />
+        <qas-btn icon-right="sym_r_more_vert" label="tertiary" variant="tertiary" />
+        <qas-btn icon="sym_r_search" variant="tertiary" />
+      </div>
+
+      <div class="q-gutter-sm q-mb-lg">
+        <qas-btn color="primary" label="tertiary" variant="tertiary" />
+        <qas-btn color="primary" icon="sym_r_search" label="tertiary" variant="tertiary" />
+        <qas-btn color="primary" icon-right="sym_r_more_vert" label="tertiary" variant="tertiary" />
+        <qas-btn color="primary" icon="sym_r_search" variant="tertiary" />
+      </div>
+
+      <div class="bg-secondary-contrast q-gutter-sm q-mb-lg q-py-lg">
+        <qas-btn color="white" label="tertiary" variant="tertiary" />
+        <qas-btn color="white" icon="sym_r_search" label="tertiary" variant="tertiary" />
+        <qas-btn color="white" icon-right="sym_r_more_vert" label="tertiary" variant="tertiary" />
+        <qas-btn color="white" icon="sym_r_search" variant="tertiary" />
+      </div>
+
+      <div class="bg-primary q-gutter-sm q-mb-lg q-py-lg">
+        <qas-btn color="white" label="tertiary" :use-hover-on-white-color="false" variant="tertiary" />
+        <qas-btn color="white" icon="sym_r_search" label="tertiary" :use-hover-on-white-color="false" variant="tertiary" />
+        <qas-btn color="white" icon-right="sym_r_more_vert" label="tertiary" :use-hover-on-white-color="false" variant="tertiary" />
+        <qas-btn color="white" icon="sym_r_search" :use-hover-on-white-color="false" variant="tertiary" />
+      </div>
+
+      <div class="q-gutter-sm">
+        <qas-label>Desabilitado</qas-label>
+        <qas-btn disabled label="tertiary" variant="tertiary" />
+        <qas-btn disabled icon="sym_r_search" label="tertiary" variant="tertiary" />
+        <qas-btn disabled icon-right="sym_r_more_vert" label="tertiary" variant="tertiary" />
+        <qas-btn disabled icon="sym_r_search" variant="tertiary" />
+      </div>
+    </div>
+  </div>
+</template>
--- ui/src/components/btn/QasBtn.vue
+++ ui/src/components/btn/QasBtn.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-btn color="primary" no-caps unelevated v-bind="attributes">
+  <q-btn class="qas-btn" v-bind="attributes">
     <slot />
 
     <template v-for="(_, name) in slots" #[name]="context">
@@ -13,7 +13,18 @@ export default {
   name: 'QasBtn',
 
   props: {
+    color: {
+      default: 'grey-9',
+      type: String,
+      validator: value => ['grey-9', 'primary', 'white'].includes(value)
+    },
+
     useLabelOnSmallScreen: {
+      default: true,
+      type: Boolean
+    },
+
+    useHoverOnWhiteColor: {
       default: true,
       type: Boolean
     },
@@ -21,15 +32,88 @@ export default {
     label: {
       default: '',
       type: String
+    },
+
+    icon: {
+      default: undefined,
+      type: String
+    },
+
+    iconRight: {
+      default: undefined,
+      type: String
+    },
+
+    loading: {
+      type: Boolean
+    },
+
+    variant: {
+      type: String,
+      default: 'primary',
+      validator: value => {
+        const variants = ['primary', 'secondary', 'tertiary']
+
+        return variants.includes(value)
+      }
     }
   },
 
   computed: {
     attributes () {
+      const {
+        dense,
+        flat,
+        glossy,
+        outline,
+        padding,
+        round,
+        unelevated,
+        ...attributes
+      } = this.$attrs
+
       return {
         ...(this.showLabel && { label: this.label }),
-        ...this.$attrs
+        class: this.classes,
+
+        ...attributes,
+        icon: this.icon,
+        iconRight: this.iconRight,
+        loading: this.loading
       }
+    },
+
+    hasIconOnly () {
+      return !this.label && (this.icon || this.iconRight)
+    },
+
+    classes () {
+      return {
+        'qas-btn--primary': this.isPrimary,
+        'qas-btn--secondary': this.isSecondary,
+        'qas-btn--tertiary': this.isTertiary,
+
+        // color
+        [`qas-btn--tertiary-${this.color}`]: this.isTertiary,
+
+        // icon
+        'qas-btn--icon-only': this.hasIconOnly,
+
+        // hover
+        'qas-btn--no-hover-on-white': !this.useHoverOnWhiteColor
+      }
+    },
+
+    isPrimary () {
+      return this.variant === 'primary'
+    },
+
+    isSecondary () {
+      return this.variant === 'secondary'
+    },
+
+    isTertiary () {
+      return this.variant === 'tertiary'
     },
 
     showLabel () {
@@ -43,3 +127,98 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+// .qas-btn {
+//   $root: &;
+
+//   border-radius: 32px;
+//   min-height: 48px;
+//   padding: var(--qas-spacing-xs) var(--qas-spacing-lg);
+//   text-transform: initial;
+//   transition: color var(--qas-generic-transition), background-color var(--qas-generic-transition);
+
+//   &:disabled {
+//     opacity: 1 !important;
+//   }
+
+//   &:not(&--primary):not(:disabled):hover {
+//     color: var(--q-primary-contrast) !important;
+//   }
+
+//   &--primary {
+//     background-color: var(--q-primary);
+//     color: white;
+
+//     &:disabled {
+//       background-color: $grey-4;
+//       color: $grey-8;
+//     }
+
+//     &:not(:disabled):hover {
+//       background-color: var(--q-primary-contrast) !important;
+//     }
+//   }
+
+//   &--secondary {
+//     border: 1px solid currentColor;
+//     color: var(--q-primary);
+//     position: relative;
+
+//     &:disabled {
+//       color: $grey-8 !important;
+//     }
+//   }
+
+//   &--tertiary {
+//     min-height: 24px !important;
+//     padding-bottom: 0;
+//     padding-top: 0;
+
+//     &:disabled {
+//       color: $grey-8 !important;
+//     }
+
+//     &-grey-9 {
+//       color: $grey-9;
+//     }
+
+//     &-primary {
+//       color: var(--q-primary);
+//     }
+
+//     &-white {
+//       color: white;
+//     }
+//   }
+
+//   &--icon-only {
+//     padding: 0;
+
+//     &:not(#{$root}--tertiary) {
+//       height: 48px;
+//       width: 48px;
+//     }
+//   }
+
+//   &.q-btn {
+//     &::before {
+//       display: none;
+//     }
+//   }
+
+//   .q-icon.on-left {
+//     margin-right: var(--qas-spacing-xs);
+//   }
+
+//   .q-icon.on-right {
+//     margin-left: var(--qas-spacing-xs);
+//   }
+
+//   .q-ripple,
+//   .q-focus-helper {
+//     display: none;
+//   }
+// }
+
+</style>
