38b106975ea6e9fc900f11c32810b1a95c9a4418
Douglas Calora <douglascalora123@gmail.com>
2023-04-11 18:47:46 -0300

    work in progress

--- docs/src/examples/QasCopy/Basic.vue
+++ docs/src/examples/QasCopy/Basic.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="container q-py-lg">
-    <qas-copy text="Este texto vai ser copiado" />
+    <qas-copy text="Este texto muito muuuuuuuito mas muuuuuuiito grande mesmo vai ser copiado" />
   </div>
 </template>
