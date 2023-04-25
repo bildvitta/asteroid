4ca33f04e14d84129335a7b4df2bceb23793f30b
Douglas Calora <douglascalora123@gmail.com>
2023-04-25 20:38:55 -0300

    work in progress

--- ui/src/components/date/QasDate.vue
+++ ui/src/components/date/QasDate.vue
@@ -40,6 +40,11 @@ export default {
 .qas-date {
   // $
 
+  .q-date__calendar-item + .q-date__calendar-item {
+    // margin-left: 20px;
+    // margin-left: var(--qas-spacing-sm);
+  }
+
   .q-date {
     &__navigation {
       .q-btn {
@@ -50,18 +55,82 @@ export default {
           grey-9
         );
       }
+
+      display: grid;
+      grid-gap: 4px;
+      /* stylelint-disable-next-line string-quotes */
+      grid-template-areas: "month-arrow-left month month-arrow-right spacing year-arrow-left year year-arrow-right";
+      grid-template-columns: 24px 80px 24px 30px 24px 40px 24px;
+
+      > div {
+        column-gap: 10px;
+        text-align: center;
+      }
+
+      div:nth-child(1) {
+        grid-area: month-arrow-left;
+      }
+
+      div:nth-child(2) {
+        grid-area: month;
+      }
+
+      div:nth-child(3) {
+        grid-area: month-arrow-right;
+      }
+
+      div:nth-child(4) {
+        grid-area: year-arrow-left;
+      }
+
+      div:nth-child(5) {
+        grid-area: year;
+      }
+
+      div:nth-child(6) {
+        grid-area: year-arrow-right;
+      }
     }
 
     &__calendar-weekdays {
       color: $grey-8;
 
       > div {
+        @include set-typography($subtitle2);
+
         opacity: 1;
+      }
+    }
+
+    &__months,
+    &__years {
+      .q-btn {
+        @include set-button(
+          tertiary,
+          true,
+          false,
+          grey-9
+        );
+      }
+    }
+
+    &__view {
+      min-height: auto;
+    }
+
+    &__calendar-days {
+      > div {
+        // width: 24px !important;
+      }
+
+      & + & {
+        margin-left: var(--qas-spacing-md);
       }
     }
 
     &__calendar-days-container {
       color: $grey-9;
+      min-height: auto;
 
       .q-btn {
         @include set-button(
