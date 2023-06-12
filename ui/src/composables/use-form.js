f8ebf08bfab99aa154af129aa003b67486861fa5
Douglas Calora <douglascalora123@gmail.com>
2023-06-12 16:50:40 -0300

    fix: useForm

--- ui/src/composables/use-form.js
+++ ui/src/composables/use-form.js
@@ -4,14 +4,12 @@ import { useRoute } from 'vue-router'
 export default function () {
   const route = useRoute()
 
-  console.log(useRoute)
-
   const isEditMode = computed(() => {
     return ['replace', 'update'].includes(mode.value)
   })
 
   const mode = computed(() => {
-    return route.name.endsWith('Edit') ? 'replace' : 'create'
+    return route.name?.endsWith('Edit') ? 'replace' : 'create'
   })
 
   return {
