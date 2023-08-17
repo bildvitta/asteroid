4afc9a8bf07ed62aecd517d56402836daa2d048e
Felipe Goulart <luisfelipegoulart94@gmail.com>
2023-08-17 20:45:54 -0300

    feat(ui): add use cached filter composable

--- ui/src/composables/index.js
+++ ui/src/composables/index.js
@@ -1,3 +1,5 @@
 export { default as useHistory } from './use-history.js'
 export { default as useForm } from './use-form.js'
 export { default as useScreen } from './use-screen.js'
+
+export * as useCachedFilter from './use-cached-filters.js'
