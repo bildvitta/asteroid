81022979f33b60b11aee98b058fd22256de944f4
Felipe Goulart <luisfelipegoulart94@gmail.com>
2023-08-18 15:13:40 -0300

    feat: rename useCachedFilters to useQueryCache

--- ui/src/composables/index.js
+++ ui/src/composables/index.js
@@ -1,6 +1,5 @@
 export { default as useContext } from './use-context.js'
-export { default as useHistory } from './use-history.js'
 export { default as useForm } from './use-form.js'
+export { default as useHistory } from './use-history.js'
+export { default as useQueryCache } from './use-query-cache.js'
 export { default as useScreen } from './use-screen.js'
-
-export * as useCachedFilter from './use-cached-filters.js'
---
+++ ui/src/composables/use-query-cache.js
@@ -0,0 +1,53 @@
+import { SessionStorage } from 'quasar'
+import { filterObject } from '../helpers'
+
+const cachedFilters = SessionStorage.getItem('cachedFilters') || {}
+
+function updateSessionStorage () {
+  SessionStorage.set('cachedFilters', cachedFilters)
+}
+
+export default function () {
+  function addOne (key, { label, value }) {
+    cachedFilters[key][label] = value
+    updateSessionStorage()
+  }
+
+  function addMany (key, filters) {
+    cachedFilters[key] = filters
+    updateSessionStorage()
+  }
+
+  function findOne (key, filter) {
+    return cachedFilters[key][filter]
+  }
+
+  function findAll (key) {
+    return cachedFilters[key]
+  }
+
+  function clearOne (key, filter) {
+    delete cachedFilters[key][filter]
+    updateSessionStorage()
+  }
+
+  function clearAll (key, { exclude = [] }) {
+    if (exclude.length) {
+      cachedFilters[key] = filterObject(cachedFilters[key], exclude)
+    } else {
+      delete cachedFilters[key]
+    }
+
+    updateSessionStorage()
+  }
+
+  return {
+    addOne,
+    addMany,
+    findOne,
+    findAll,
+    clearOne,
+    clearAll,
+    cachedFilters
+  }
+}
