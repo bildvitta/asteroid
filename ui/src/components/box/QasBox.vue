40389d03e62d50c4cb3b25407c39efb069e756c9
gcarpi <gcarpi@outlook.com>
2023-08-28 11:57:23 -0300

    feat: QasBox two new props: paddingHorizontal and paddingVertical

--- ui/src/components/box/QasBox.vue
+++ ui/src/components/box/QasBox.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="bg-white q-pa-lg rounded-borders-lg" :class="boxClass">
+  <div :class="boxClass">
     <slot />
   </div>
 </template>
 
 <script setup>
 import { computed, inject, provide } from 'vue'
+import { Spacing } from '../../enums/Spacing'
 
 defineOptions({
   name: 'QasBox'
@@ -14,6 +15,18 @@ defineOptions({
 const props = defineProps({
   outlined: {
     type: Boolean
+  },
+
+  paddingHorizontal: {
+    default: Spacing.Lg,
+    type: String,
+    validator: value => Object.values(Spacing).includes(value)
+  },
+
+  paddingVertical: {
+    default: Spacing.Lg,
+    type: String,
+    validator: value => Object.values(Spacing).includes(value)
   },
 
   unelevated: {
@@ -25,8 +38,15 @@ provide('hasParent', true)
 
 const hasParent = inject('hasParent', false)
 
-const boxClass = computed(() => ({
-  'border-grey': props.outlined || hasParent,
-  'shadow-2': !props.unelevated && !hasParent
-}))
+const boxClass = computed(() => {
+  return [
+    'bg-white rounded-borders-lg full-width',
+    'q-px-' + props.paddingHorizontal,
+    'q-py-' + props.paddingVertical,
+    {
+      'border-grey': props.outlined || hasParent,
+      'shadow-2': !props.unelevated && !hasParent
+    }
+  ]
+})
 </script>
