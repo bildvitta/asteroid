130a3c6c384dbccefc5ebfb24406a6c6f65a4240
gcarpi <gcarpi@outlook.com>
2023-12-01 18:53:19 -0300

    wip: QasFilters refactor to use composition api

--- ui/src/components/filters/QasFilters.vue
+++ ui/src/components/filters/QasFilters.vue
@@ -3,11 +3,11 @@
     <div v-if="showFilters" class="q-col-gutter-x-md row">
       <div v-if="showSearch" class="col-12 col-md-6">
         <slot :filter="filter" name="search">
-          <q-form v-if="useSearch" @submit.prevent="filter()">
-            <qas-search-input v-model="search" :placeholder="searchPlaceholder" :use-search-on-type="useSearchOnType" @clear="clearSearch" @filter="filter()" @update:model-value="onSearch">
+          <q-form v-if="props.useSearch" @submit.prevent="filter()">
+            <qas-search-input v-model="search" :placeholder="props.searchPlaceholder" :use-search-on-type="props.useSearchOnType" @clear="clearSearch" @filter="filter()" @update:model-value="onSearch">
               <template v-if="showFilterButton" #after-clear>
-                <slot :context="mx_context" :filter="filter" :filters="activeFilters" name="filter-button" :remove-filter="removeFilter">
-                  <pv-filters-button v-if="useFilterButton" ref="filtersButton" v-model="filters" v-bind="filterButtonProps" />
+                <slot :context="context" :filter="filter" :filters="activeFilters" name="filter-button" :remove-filter="removeFilter">
+                  <pv-filters-button v-if="props.useFilterButton" ref="filtersButton" v-model="filters" v-bind="filterButtonProps" />
                 </slot>
               </template>
             </qas-search-input>
@@ -16,8 +16,8 @@
       </div>
 
       <div v-else-if="showFilterButton" class="col-12 col-md-6">
-        <slot :context="mx_context" :filter="filter" :filters="activeFilters" name="filter-button" :remove-filter="removeFilter">
-          <pv-filters-button v-if="useFilterButton" ref="filtersButton" v-model="filters" v-bind="filterButtonProps" />
+        <slot :context="context" :filter="filter" :filters="activeFilters" name="filter-button" :remove-filter="removeFilter">
+          <pv-filters-button v-if="props.useFilterButton" ref="filtersButton" v-model="filters" v-bind="filterButtonProps" />
         </slot>
       </div>
 
@@ -33,367 +33,371 @@
       </q-chip>
     </div>
 
-    <slot :context="mx_context" :filter="filter" :filters="activeFilters" :remove-filter="removeFilter" />
+    <slot :context="context" :filter="filter" :filters="activeFilters" :remove-filter="removeFilter" />
   </section>
 </template>
 
-<script>
+<script setup>
+import { ref, computed, useSlots, watch, getCurrentInstance } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { useContext } from '../../composables'
+import { camelize, camelizeKeys, decamelize } from 'humps'
+import { humanize, parseValue } from '../../helpers/filters.js'
+import { getState, getAction } from '@bildvitta/store-adapter'
 import PvFiltersButton from './private/PvFiltersButton.vue'
 import debug from 'debug'
 
-import { camelize, camelizeKeys, decamelize } from 'humps'
-import { humanize, parseValue } from '../../helpers/filters.js'
-import contextMixin from '../../mixins/context.js'
-import { getState, getAction } from '@bildvitta/store-adapter'
-
 const log = debug('asteroid-ui:qas-filters')
 
-export default {
-  name: 'QasFilters',
+const { context } = useContext()
+const route = useRoute()
+const router = useRouter()
 
-  components: {
-    PvFiltersButton
+// TODO: Rever uso do getCurrentInstance()
+const app = getCurrentInstance().appContext.config.globalProperties
+
+defineOptions({ name: 'QasFilters' })
+
+// Props
+const props = defineProps({
+  useChip: {
+    default: true,
+    type: Boolean
   },
 
-  mixins: [contextMixin],
-
-  props: {
-    useChip: {
-      default: true,
-      type: Boolean
-    },
-
-    entity: {
-      required: true,
-      type: String
-    },
-
-    fieldsProps: {
-      default: () => ({}),
-      type: Object
-    },
-
-    useFilterButton: {
-      default: true,
-      type: Boolean
-    },
-
-    useSearch: {
-      default: true,
-      type: Boolean
-    },
-
-    useSearchOnType: {
-      default: true,
-      type: Boolean
-    },
-
-    searchPlaceholder: {
-      default: 'Pesquisar...',
-      type: String
-    },
-
-    url: {
-      default: '',
-      type: String
-    },
-
-    useForceRefetch: {
-      type: Boolean
-    },
-
-    useSpacing: {
-      default: true,
-      type: Boolean
-    },
-
-    useUpdateRoute: {
-      default: true,
-      type: Boolean
-    }
+  entity: {
+    required: true,
+    type: String
   },
 
-  emits: ['fetch-success', 'fetch-error', 'update:currentFilters'],
-
-  data () {
-    return {
-      currentFilters: {},
-      filters: {},
-      hasFetchError: false,
-      isFetching: false,
-      search: ''
-    }
+  fieldsProps: {
+    default: () => ({}),
+    type: Object
   },
 
-  computed: {
-    activeFilters () {
-      if (!this.hasFields) {
-        return {}
-      }
-
-      const activeFilters = {}
-
-      const fields = Object.keys(this.fields)
-      const filters = camelizeKeys(this.useUpdateRoute ? this.mx_context.filters : this.currentFilters)
-
-      for (const key in filters) {
-        const hasField = fields.includes(key)
-
-        if (hasField) {
-          const field = { ...this.fields[key], ...this.formattedFieldsProps?.[decamelize(key)] }
-          const value = humanize(field, this.normalizeValues(filters[key], field?.multiple))
-
-          if (!value) continue
-
-          const { label, name } = field
-
-          activeFilters[key] = { label, name, value }
-        }
-      }
-
-      return activeFilters
-    },
-
-    formattedFieldsProps () {
-      const fieldsProps = {}
-
-      for (const key in this.fieldsProps) {
-        const decamelizedFieldKey = decamelize(key)
-
-        fieldsProps[decamelizedFieldKey] = { ...this.fieldsProps[key] }
-      }
-
-      return fieldsProps
-    },
-
-    fields () {
-      return getState.call(this, { entity: this.entity, key: 'filters' })
-    },
-
-    filtersClass () {
-      return {
-        'q-mb-xl': this.useSpacing
-      }
-    },
-
-    filterButtonColor () {
-      return this.hasActiveFilters ? 'primary' : 'grey-9'
-    },
-
-    filterButtonProps () {
-      return {
-        color: this.filterButtonColor,
-        error: this.hasFetchError,
-        fields: this.fields,
-        fieldsProps: this.formattedFieldsProps,
-        loading: this.isFetching,
-
-        onClear: this.clearFilters,
-        onFilter: () => this.filter()
-      }
-    },
-
-    hasActiveFilters () {
-      return !!Object.keys(this.activeFilters).length
-    },
-
-    hasFields () {
-      return !!Object.keys(this.fields || {}).length
-    },
-
-    showFilterButton () {
-      return !!this.$slots.filterButton || this.useFilterButton
-    },
-
-    showFilters () {
-      return this.useFilterButton || this.showSearch
-    },
-
-    showSearch () {
-      return !!this.$slots.search || this.useSearch
-    },
-
-    hasChip () {
-      return this.useChip && this.hasActiveFilters
-    }
+  useFilterButton: {
+    default: true,
+    type: Boolean
   },
 
-  watch: {
-    $route (to, from) {
-      if (to.name === from.name) {
-        this.fetchFilters()
-        this.useUpdateRoute && this.updateValues()
-      }
-    }
+  useSearch: {
+    default: true,
+    type: Boolean
   },
 
-  async created () {
-    await this.fetchFilters()
-
-    if (this.useUpdateRoute) {
-      this.updateValues()
-      this.updateCurrentFilters()
-    }
-
-    this.handleSearchModelOnCreate()
+  useSearchOnType: {
+    default: true,
+    type: Boolean
   },
 
-  methods: {
-    async clearFilters () {
-      const { filters } = this.mx_context
-      const query = { ...this.$route.query }
-      const activeFilters = {
-        ...filters,
-        ...this.filters
-      }
+  searchPlaceholder: {
+    default: 'Pesquisar...',
+    type: String
+  },
 
-      if (this.hasFields) {
-        const fields = Object.keys(this.fields)
+  url: {
+    default: '',
+    type: String
+  },
 
-        for (const key in activeFilters) {
-          const camelizedKey = camelize(key)
-          const hasField = fields.includes(camelizedKey)
+  useForceRefetch: {
+    type: Boolean
+  },
 
-          if (hasField) {
-            delete query[key]
-            delete this.filters[key]
-          }
-        }
-      } else {
-        this.filters = {}
-      }
+  useSpacing: {
+    default: true,
+    type: Boolean
+  },
 
-      this.hideFiltersMenu()
+  useUpdateRoute: {
+    default: true,
+    type: Boolean
+  }
+})
 
-      await this.updateRouteQuery(query)
+// Emitters
+const emits = defineEmits(['fetch-success', 'fetch-error', 'update:currentFilters'])
 
-      this.updateCurrentFilters()
-    },
+// Slots
+const slots = useSlots()
 
-    clearSearch () {
-      this.search = ''
-      this.filter()
-    },
+// Refs
+const currentFilters = ref({})
+const filters = ref({})
+const filtersButton = ref(null)
+const hasFetchError = ref(false)
+const isFetching = ref(false)
+const search = ref('')
 
-    async fetchFilters () {
-      if (!this.useForceRefetch && (this.hasFields || !this.useFilterButton)) {
-        return null
-      }
+// Computadas
+const activeFilters = computed(() => {
+  if (!hasFields.value) {
+    return {}
+  }
 
-      this.hasFetchError = false
-      this.isFetching = true
+  const activeFilters = {}
 
-      try {
-        const response = await getAction.call(this, {
-          entity: this.entity,
-          key: 'fetchFilters',
-          payload: { url: this.url }
-        })
+  const fieldsKeys = Object.keys(fields.value)
+  const filters = camelizeKeys(props.useUpdateRoute ? context.value.filters : currentFilters.value)
 
-        this.$emit('fetch-success', response)
+  for (const key in filters) {
+    const hasField = fieldsKeys.includes(key)
 
-        log(`[${this.entity}]:fetchFilters:success`, response)
-      } catch (error) {
-        this.hasFetchError = true
-        this.$emit('fetch-error', error)
+    if (hasField) {
+      const field = { ...fields.value[key], ...formattedFieldsProps.value?.[decamelize(key)] }
+      const value = humanize(field, normalizeValues(filters[key], field?.multiple))
 
-        log(`[${this.entity}]:fetchFilters:error`, error)
-      } finally {
-        this.isFetching = false
-      }
-    },
+      if (!value) continue
 
-    async filter (external) {
-      const { filters, page, ...context } = this.mx_context
+      const { label, name } = field
 
-      const query = {
-        ...filters,
-        ...this.filters,
-        ...external,
-        ...context,
-        search: this.search || undefined
-      }
-
-      for (const key in query) {
-        query[key] ?? delete query[key]
-      }
-
-      this.hideFiltersMenu()
-
-      await this.updateRouteQuery(query)
-
-      this.updateCurrentFilters()
-    },
-
-    getChipValue (value) {
-      return Array.isArray(value) ? value.join(', ') : value
-    },
-
-    hideFiltersMenu () {
-      this.$refs.filtersButton?.hideMenu()
-    },
-
-    async removeFilter ({ name }) {
-      const query = { ...this.$route.query }
-
-      delete query[name]
-      delete this.filters[name]
-
-      await this.updateRouteQuery(query)
-
-      this.updateCurrentFilters()
-    },
-
-    updateCurrentFilters () {
-      this.currentFilters = {
-        ...this.filters,
-        ...(this.search && { search: this.search })
-      }
-
-      this.$emit('update:currentFilters', this.currentFilters)
-    },
-
-    async updateRouteQuery (query) {
-      this.useUpdateRoute && await this.$router.push({ query })
-    },
-
-    updateValues () {
-      this.setSearch()
-      this.setFilters()
-    },
-
-    normalizeValues (value, isMultiple) {
-      if (Array.isArray(value)) return value
-
-      return isMultiple ? [value] : value
-    },
-
-    handleSearchModelOnCreate () {
-      if (this.useUpdateRoute && !this.useFilterButton) {
-        this.setSearch()
-      }
-    },
-
-    onSearch () {
-      if (this.useSearchOnType) {
-        this.filter()
-      }
-    },
-
-    setSearch () {
-      const { search } = this.mx_context
-      this.search = search || ''
-    },
-
-    setFilters () {
-      this.filters = {}
-
-      const { filters } = this.mx_context
-
-      for (const key in filters) {
-        this.filters[key] = parseValue(this.normalizeValues(filters[key], this.fields[key]?.multiple))
-      }
+      activeFilters[key] = { label, name, value }
     }
   }
+
+  return activeFilters
+})
+
+const formattedFieldsProps = computed(() => {
+  const fieldsProps = {}
+
+  for (const key in props.fieldsProps) {
+    const decamelizedFieldKey = decamelize(key)
+
+    fieldsProps[decamelizedFieldKey] = { ...props.fieldsProps[key] }
+  }
+
+  return fieldsProps
+})
+
+const fields = computed(() => {
+  return getState.call(app, { entity: props.entity, key: 'filters' })
+})
+
+const filtersClass = computed(() => {
+  return {
+    'q-mb-xl': props.useSpacing
+  }
+})
+
+const filterButtonColor = computed(() => {
+  return hasActiveFilters.value ? 'primary' : 'grey-9'
+})
+
+const filterButtonProps = computed(() => {
+  return {
+    color: filterButtonColor.value,
+    error: hasFetchError.value,
+    fields: fields.value,
+    fieldsProps: formattedFieldsProps.value,
+    loading: isFetching.value,
+
+    onClear: clearFilters,
+    onFilter: () => filter()
+  }
+})
+
+const hasActiveFilters = computed(() => {
+  return !!Object.keys(activeFilters.value).length
+})
+
+const hasFields = computed(() => {
+  return !!Object.keys(fields.value || {}).length
+})
+
+const showFilterButton = computed(() => {
+  return !!slots.filterButton || props.useFilterButton
+})
+
+const showFilters = computed(() => {
+  return props.useFilterButton || showSearch.value
+})
+
+const showSearch = computed(() => {
+  return !!slots.search || props.useSearch
+})
+
+const hasChip = computed(() => {
+  return props.useChip && hasActiveFilters.value
+})
+
+// Watchers
+watch(
+  () => route,
+  (to, from) => {
+    if (to.name === from.name) {
+      fetchFilters()
+
+      props.useUpdateRoute && updateValues()
+    }
+  }
+)
+
+// Métodos
+async function init () {
+  await fetchFilters()
+
+  if (props.useUpdateRoute) {
+    updateValues()
+    updateCurrentFilters()
+  }
+
+  handleSearchModelOnCreate()
 }
+
+async function clearFilters () {
+  const query = { ...route.query }
+
+  const activeFilters = {
+    ...context.value.filters,
+    ...filters.value
+  }
+
+  if (hasFields.value) {
+    const fieldsKeys = Object.keys(fields.value)
+
+    for (const key in activeFilters) {
+      const camelizedKey = camelize(key)
+      const hasField = fieldsKeys.includes(camelizedKey)
+
+      if (hasField) {
+        delete query[key]
+        delete filters.value[key]
+      }
+    }
+  } else {
+    filters.value = {}
+  }
+
+  hideFiltersMenu()
+
+  await updateRouteQuery(query)
+
+  updateCurrentFilters()
+}
+
+function clearSearch () {
+  search.value = ''
+
+  filter()
+}
+
+async function fetchFilters () {
+  if (!props.useForceRefetch && (hasFields.value || !props.useFilterButton)) {
+    return null
+  }
+
+  hasFetchError.value = false
+  isFetching.value = true
+
+  try {
+    const response = await getAction.call(app, {
+      entity: props.entity,
+      key: 'fetchFilters',
+      payload: { url: props.url }
+    })
+
+    emits('fetch-success', response)
+
+    log(`[${props.entity}]:fetchFilters:success`, response)
+  } catch (error) {
+    hasFetchError.value = true
+    emits('fetch-error', error)
+
+    log(`[${props.entity}]:fetchFilters:error`, error)
+  } finally {
+    isFetching.value = false
+  }
+}
+
+async function filter (external) {
+  const { filters: contextFilters, page, ...rest } = context.value
+
+  const query = {
+    ...contextFilters,
+    ...filters.value,
+    ...external,
+    ...rest,
+    search: search.value || undefined
+  }
+
+  for (const key in query) {
+    query[key] ?? delete query[key]
+  }
+
+  hideFiltersMenu()
+
+  await updateRouteQuery(query)
+
+  updateCurrentFilters()
+}
+
+function getChipValue (value) {
+  return Array.isArray(value) ? value.join(', ') : value
+}
+
+function hideFiltersMenu () {
+  filtersButton.value?.hideMenu()
+}
+
+async function removeFilter ({ name }) {
+  const query = { ...route.query }
+
+  delete query[name]
+  delete filters.value[name]
+
+  await updateRouteQuery(query)
+
+  updateCurrentFilters()
+}
+
+function updateCurrentFilters () {
+  currentFilters.value = {
+    ...filters.value,
+    ...(search.value && { search: search.value })
+  }
+
+  emits('update:currentFilters', currentFilters.value)
+}
+
+async function updateRouteQuery (query) {
+  return props.useUpdateRoute && await router.push({ query })
+}
+
+function updateValues () {
+  setSearch()
+  setFilters()
+}
+
+function normalizeValues (value, isMultiple) {
+  if (Array.isArray(value)) return value
+
+  return isMultiple ? [value] : value
+}
+
+function handleSearchModelOnCreate () {
+  if (props.useUpdateRoute && !props.useFilterButton) {
+    setSearch()
+  }
+}
+
+function onSearch () {
+  if (props.useSearchOnType) {
+    filter()
+  }
+}
+
+function setSearch () {
+  search.value = context.value.search || ''
+}
+
+function setFilters () {
+  filters.value = {}
+
+  for (const key in context.value.filters) {
+    filters.value[key] = parseValue(normalizeValues(context.value.filters[key], fields.value[key]?.multiple))
+  }
+}
+
+init()
 </script>
