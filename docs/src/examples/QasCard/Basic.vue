ed67309cba0c8b546423c7623cc73c3a8a7ee5de
felipejuzo02 <felipejuzo@hotmail.com>
2024-02-06 19:29:29 -0300

    doc: qas-card and qas-card-image docs

--- docs/src/examples/QasCard/Basic.vue
+++ docs/src/examples/QasCard/Basic.vue
@@ -1,21 +1,38 @@
 <template>
   <div class="container spaced">
     <div class="q-col-gutter-md row">
-      <qas-card :images="images" use-header>
+      <qas-card
+        :route="{ name: 'Root'}"
+        title="Titulo"
+      >
         <template #default>
           <div>
             Meu conteúdo
           </div>
         </template>
       </qas-card>
-      <qas-card :images="images" use-header>
+
+      <qas-card
+        expansion-content="Conteúdo do menu expansivo"
+        :route="{ name: 'Root'}"
+        title="Titulo"
+        use-expansion
+      >
         <template #default>
           <div>
             Meu conteúdo
           </div>
         </template>
       </qas-card>
-      <qas-card :images="images" use-header>
+
+      <qas-card
+        :actions-menu-props="actionsMenuProps"
+        expansion-content="Conteúdo do menu expansivo"
+        :route="{ name: 'Root'}"
+        status-color="#c42644"
+        title="Titulo"
+        use-expansion
+      >
         <template #default>
           <div>
             Meu conteúdo
@@ -29,21 +46,18 @@
 <script>
 export default {
   computed: {
-    images () {
-      return [
-        'https://via.placeholder.com/200x300',
-        'https://via.placeholder.com/200x300',
-        'https://via.placeholder.com/200x300',
-        'https://via.placeholder.com/200x300',
-        'https://via.placeholder.com/200x300',
-        'https://via.placeholder.com/200x300',
-        'https://via.placeholder.com/200x300',
-        'https://via.placeholder.com/200x300',
-        'https://via.placeholder.com/200x300',
-        'https://via.placeholder.com/200x300',
-        'https://via.placeholder.com/200x300',
-        'https://via.placeholder.com/200x300'
-      ]
+    actionsMenuProps () {
+      return {
+        show: {
+          label: 'Ir para detalhes',
+          icon: 'sym_r_visibility'
+        },
+
+        edit: {
+          label: 'Editar',
+          icon: 'sym_r_edit'
+        }
+      }
     }
   }
 }
---
+++ docs/src/pages/components/card-image.md
@@ -0,0 +1,11 @@
+---
+title: QasCardImage
+---
+
+Componente de card.
+
+<doc-api file="card-image/QasCardImage" name="QasCardImage" />
+
+## Uso
+
+<doc-example file="QasCardImage/Basic" title="Básico" />
