4e3c24478fde7e0b62247657015e3d8acfa8c61a
felipejuzo02 <felipejuzo@hotmail.com>
2024-04-29 16:03:23 -0300

    refactor: doc update

---
+++ docs/src/examples/QasChartView/WithCustomColors.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container spaced">
+    <qas-chart-view :colors-list="colorsList" entity="charts_multiple_results" type="bar" />
+  </div>
+</template>
+
+<script setup>
+defineOptions({ name: 'QasChartViewWithCustomColors' })
+
+const colorsList = [
+  '#FFB200',
+  '#F62D1B'
+]
+</script>
