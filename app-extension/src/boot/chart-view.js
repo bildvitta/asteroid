e4626531db6726a98c4deeccb2f188308459662b
Douglas Calora <douglascalora123@gmail.com>
2024-08-07 10:35:33 -0300

    feat: Update import path for QasChartView component

--- app-extension/src/boot/chart-view.js
+++ app-extension/src/boot/chart-view.js
@@ -3,9 +3,7 @@ import thirdPartyComponentHandler from '@bildvitta/quasar-ui-asteroid/src/vue-pl
 export default async ({ app }) => {
   const thirdParty = thirdPartyComponentHandler({
     name: 'QasChartView',
-    // TODO: Voltar código comentado antes de publicar
-    importFn: () => import('../../../ui/src/components/chart-view/QasChartView.vue')
-    // importFn: () => import('@bildvitta/quasar-ui-asteroid/src/components/chart-view/QasChartView.vue')
+    importFn: () => import('@bildvitta/quasar-ui-asteroid/src/components/chart-view/QasChartView.vue')
   })
 
   thirdParty.initializeComponent({ app })
