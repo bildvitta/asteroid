2b775daf09bcf1e824e1bb02dfcfb155fb74cbe6
Douglas Calora <douglascalora123@gmail.com>
2024-08-13 14:41:20 -0300

    feat: Update QasToggle component to use v-model for two-way data binding

--- docs/src/examples/QasToggle/Basic.vue
+++ docs/src/examples/QasToggle/Basic.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="container spaced">
-    <qas-toggle label="Aceitar" :model-value="false" />
+    <qas-toggle v-model="model" label="Aceitar" />
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+
 defineOptions({ name: 'Basic' })
+
+const model = ref(false)
 </script>
