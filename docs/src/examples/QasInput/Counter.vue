274d88ea72a85ff032e373f15756d7b86ee61b8c
felipejuzo02 <felipejuzo@hotmail.com>
2024-08-20 10:59:45 -0300

    doc: update input doc

---
+++ docs/src/examples/QasInput/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container q-col-gutter-sm q-py-lg row">
+    <qas-input v-model="model" class="col-12" :counter label="Meu input" :maxlength>
+      <template #prepend>
+        <q-icon name="sym_r_search" size="md" />
+      </template>
+
+      <template #append>
+        <qas-btn icon="sym_r_search" />
+      </template>
+    </qas-input>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+defineOptions({ name: 'Counter' })
+
+const counter = true
+const maxlength = 12
+
+const model = ref('')
+
+</script>
