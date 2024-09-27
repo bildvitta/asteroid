07c3c52a8592a4a04889ab242c01c4fb76593121
Douglas Calora <douglascalora123@gmail.com>
2024-09-27 18:57:12 -0300

    refactor: update QasHeader component to support dynamic actions component

---
+++ docs/src/examples/QasHeader/HeaderWithFilters.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container spaced">
+    <qas-header v-bind="headerProps" />
+  </div>
+</template>
+
+<script setup>
+const headerProps = {
+  filtersProps: {
+    entity: 'users'
+  },
+
+  labelProps: {
+    label: 'Com filtro'
+  },
+
+  description: 'lorem ipsum dolor sit amet consectetur adipiscing elit'
+}
+</script>
