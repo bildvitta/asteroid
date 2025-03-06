cc6bbda03fbafe6c457ee56a88aca567024abc14
Douglas Calora <douglascalora123@gmail.com>
2025-03-06 10:32:19 -0300

    feat: add new example for QasTextTruncate component inside a table

---
+++ docs/src/examples/QasTextTruncate/InsideTable.vue
@@ -0,0 +1,19 @@
+<template>
+  <qas-list-view v-model:fields="viewState.fields" v-model:results="viewState.results" :entity="entity" :use-filter="false">
+    <template #default>
+      <qas-table-generator :fields="viewState.fields" :results="viewState.results" row-key="uuid">
+        <template #body-cell-name="{ row }">
+          <qas-text-truncate dialog-title="Aqui está meu título!" :max-width="180" :text="row.name" />
+        </template>
+      </qas-table-generator>
+    </template>
+  </qas-list-view>
+</template>
+
+<script setup>
+import { useView } from '@bildvitta/composables'
+
+const { viewState } = useView({ mode: 'list' })
+
+const entity = 'users'
+</script>
