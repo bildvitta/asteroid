d12388267105fa5bbb8f624586a82fb9eb777939
Douglas Calora <douglascalora123@gmail.com>
2025-03-06 10:33:41 -0300

    fix: update dialog title in QasTextTruncate example inside table

--- docs/src/examples/QasTextTruncate/InsideTable.vue
+++ docs/src/examples/QasTextTruncate/InsideTable.vue
@@ -1,9 +1,9 @@
 <template>
-  <qas-list-view v-model:fields="viewState.fields" v-model:results="viewState.results" :entity="entity" :use-filter="false">
+  <qas-list-view v-model:fields="viewState.fields" v-model:results="viewState.results" :entity :use-filter="false">
     <template #default>
       <qas-table-generator :fields="viewState.fields" :results="viewState.results" row-key="uuid">
         <template #body-cell-name="{ row }">
-          <qas-text-truncate dialog-title="Aqui está meu título!" :max-width="180" :text="row.name" />
+          <qas-text-truncate dialog-title="Nome" :max-width="180" :text="row.name" />
         </template>
       </qas-table-generator>
     </template>
