26e1ef6665ce7ca1ca31c9be7d4caeedb29ba8e6
eliasassuncao <elias3748@gmail.com>
2025-04-28 16:11:48 -0300

    feat: fix list actions menu

--- ui/src/components/actions-menu/QasActionsMenu.vue
+++ ui/src/components/actions-menu/QasActionsMenu.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-if="hasList" class="qas-actions-menu" data-cy="actions-menu">
     <qas-btn-dropdown v-bind="btnDropdownProps">
-      <q-list data-cy="actions-menu-list">
+      <q-list v-if="hasDropdownListLength" data-cy="actions-menu-list">
         <slot v-for="(item, key) in formattedList.dropdownList" :item="item" :name="key">
           <q-item v-bind="getItemProps(item)" :key="key" active-class="primary" clickable data-cy="actions-menu-list-item" @click="setClickHandler(item)">
             <q-item-section avatar>
@@ -156,6 +156,8 @@ const defaultButtonPropsList = computed(() => {
 
   return normalizedButtonPropsList
 })
+
+const hasDropdownListLength = computed(() => formattedList.value.dropdownList?.length)
 
 const btnDropdownProps = computed(() => {
   return {
