257cb68dbee57df3a8f059505f98469a5115c99f
Douglas Calora <douglascalora123@gmail.com>
2025-05-21 14:26:15 -0300

    Releasing v3.19.0-alpha.0

--- build/release/release-ui.js
+++ build/release/release-ui.js
@@ -2,12 +2,15 @@ function releaseUi ({ execaSync, ora, publishCommands, packages }) {
   const publishSpinner = ora('Publicando "ui"...').start()
 
   try {
+    // reseta o cache do npm
+    execaSync('npm', ['cache', 'clean', '--force'])
+
     execaSync('npm', publishCommands, { cwd: packages.ui.resolved })
     publishSpinner.succeed('"ui" publicada')
 
     return { success: true, error: false }
-  } catch {
-    publishSpinner.fail('Falha ao publicar "ui"')
+  } catch (error) {
+    publishSpinner.fail(`Falha ao publicar "ui" - ${error.message}'`)
     return { success: false, error: true }
   }
 }
