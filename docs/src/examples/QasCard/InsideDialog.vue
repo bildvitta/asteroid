cc158558ebe761438bb25b41dfff73a77b6e8d7c
felipejuzo02 <felipejuzo@hotmail.com>
2025-07-25 14:21:06 -0300

    feat(QasCard): change style to use border when use qas-card inside dialog

---
+++ docs/src/examples/QasCard/InsideDialog.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container spaced">
+    <qas-btn
+      label="Abrir dialog"
+      @click="openDialog"
+    />
+
+    <qas-dialog v-model="showDialog">
+      <template #description>
+        <div class="q-mb-md">
+          Este exemplo de um QasCard dentro de um QasDialog.
+        </div>
+
+        <qas-card :route="{ name: 'Root' }" title="Titulo">
+          <template #default>
+            <div>
+              Meu conteúdo
+            </div>
+          </template>
+        </qas-card>
+      </template>
+    </qas-dialog>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+defineOptions({ name: 'InsideDialog' })
+
+// refs
+const showDialog = ref(false)
+
+// functions
+function openDialog () {
+  showDialog.value = true
+}
+</script>
