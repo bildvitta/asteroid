7e614814456713a038d4824c5ba65b61c7996c74
Douglas Calora <douglascalora123@gmail.com>
2025-08-13 15:37:59 -0300

    feat: integrate selection functionality in QasTableGenerator and update example component

--- docs/src/examples/QasTableGenerator/Selection.vue
+++ docs/src/examples/QasTableGenerator/Selection.vue
@@ -1,18 +1,45 @@
 <template>
-  <qas-list-view v-model:fields="fields" v-model:results="results" :entity="entity" :use-filter="false">
+  <qas-list-view v-model:fields="viewState.fields" v-model:results="viewState.results" :entity :use-filter="false">
     <template #default>
-      <qas-table-generator :fields="fields" :results="results" row-key="uuid" />
+      <qas-table-generator v-model:selected="selectedUsers" v-bind="tableGeneratorProps" />
+
+      <qas-debugger :inspect="[selectedUsers]" />
+
+      <qas-btn label="Resetar" @click="reset" />
     </template>
   </qas-list-view>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { useView } from '@bildvitta/composables'
+import { computed, ref } from 'vue'
 
 defineOptions({ name: 'Selection' })
 
+// composables
+const { viewState } = useView({ mode: 'list' })
+
+// refs
+const selectedUsers = ref([])
+
+// consts
 const entity = 'users'
 
-const fields = ref({})
-const results = ref([])
+// computeds
+const tableGeneratorProps = computed(() => {
+  return {
+    fields: viewState.value.fields,
+    results: viewState.value.results,
+    rowKey: 'uuid',
+    useSelection: true
+  }
+})
+
+// functions
+function reset () {
+  selectedUsers.value = [
+    '2f8856d0-8eca-4e41-8146-63ed2a4f23ff4c',
+    '943e4923-12c0-473e-a07f-63eb28201a91-24'
+  ]
+}
 </script>
