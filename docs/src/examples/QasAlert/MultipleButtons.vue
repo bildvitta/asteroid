8fc1a9283ef3fcd9c9b6e09113191d06418072eb
felipejuzo02 <felipejuzo@hotmail.com>
2025-10-02 17:49:48 -0300

    feat(QasAlert) insert usage with multiple btns

---
+++ docs/src/examples/QasAlert/MultipleButtons.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container q-py-lg">
+    <qas-alert v-bind="multipleButtonsAlertProps" />
+
+    <qas-alert v-bind="multipleLinksAlertProps" class="q-mt-md" />
+  </div>
+</template>
+
+<script setup>
+defineOptions({ name: 'MultipleButtons' })
+
+const multipleButtonsAlertProps = {
+  useRegex: true,
+  text: 'Exemplo com [botão 1], e também com [botão 2]',
+  buttonProps: [
+    {
+      onClick: () => alert('Botão 1 clicado')
+    },
+    {
+      onClick: () => alert('Botão 2 clicado')
+    }
+  ]
+}
+
+const multipleLinksAlertProps = {
+  useRegex: true,
+  text: 'Vou redirecionar para [home], e aqui para a [página de alertas]',
+  routerLinkProps: [
+    {
+      to: '/'
+    },
+    {
+      to: '/components/alert'
+    }
+  ]
+}
+</script>
