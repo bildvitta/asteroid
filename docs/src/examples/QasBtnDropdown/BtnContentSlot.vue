c62952273803afdb6194f0f1ab60c43eac1e00d9
Douglas Calora <douglascalora123@gmail.com>
2025-11-07 17:27:09 -0300

    feat: add BtnContentSlot component and update documentation in btn-dropdown.md

---
+++ docs/src/examples/QasBtnDropdown/BtnContentSlot.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container q-py-lg">
+    <qas-btn-dropdown :buttons-props-list="buttonsPropsList">
+      <template #btn-content-menu>
+        <q-menu>
+          <q-list>
+            <q-item v-for="item in 5" :key="item">
+              Opção de exportação - {{ item }}
+            </q-item>
+          </q-list>
+        </q-menu>
+      </template>
+    </qas-btn-dropdown>
+  </div>
+</template>
+
+<script setup>
+defineOptions({ name: 'BtnContentSlot' })
+
+const buttonsPropsList = {
+  menu: {
+    label: 'Clique para abrir o QMenu',
+    icon: 'sym_r_upload',
+    color: 'primary'
+  },
+
+  sync: {
+    label: 'Sincronizar arquivos',
+    iconRight: 'sym_r_sync',
+    onClick: () => alert('Sincronizando...')
+  }
+}
+</script>
